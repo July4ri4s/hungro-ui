38089fb4f6a5cac586e54677248b0ac0abb89530
Brandon <2016467@est.cedesdonbosco.ed.cr>
2023-10-24 23:12:56 -0600

    Modifique la info del responsive

--- src/layouts/Landing/components/donations.jsx
+++ src/layouts/Landing/components/donations.jsx
@@ -1,60 +1,64 @@
-import manos from "../../../assets/manos.jpg"
+import apreton from "../../../assets/apreton-de-manos.png"
 
 const Donacion = () => {
 
     return(
         //[#195527db]
-        <section className=" w-[90%] flex flex-wrap content-center m-auto  text-[#195527] md:justify-between bg-[#195527] p-18 md:p-28 md:m-20 rounded-xl md:h-[40rem]">
-   <div className="bg-white h-[120%] flex flex-wrap rounded-xl">
-   <div className=" bg-white h-[480px] w-[38%] content-center items-center ml-3 my-auto rounded-xl">
-          <div className="  m-auto  content-center ">
+<div className="flex flex-col w-[90%]  pb-6  m-auto mt-24 text-white items-center md:justify-between bg-[#195527] p-18 md:p-28 md:m-20 rounded-xl md:h-[40rem]">
+      <div className="text-center mb-4 text-4xl  tracking-tight mt-6">
+        <h2 className="max-w-2xl mx-auto text-4xl font-semibold leading-tight text-center font-display text-white sm:text-5xl sm:leading-tight">
+         Donación
+        </h2>
+      </div>
+
+      <div className="flex flex-col md:flex-row justify-around text-center  w-full mt-10 ">
+        <div className="flex flex-col justify-center items-center">
           
-          <div  className="m-auto px-2 py-11 " >
-        <img className="w-[100%] block object-center mx-auto mt-2" src={manos} alt="dashboard image" />
+
+          <h2 className="my-4 font-semibold text-4xl">¿Para quien?</h2>
+          <p className=" md:w-44 ">
+           Para todo el que realmente necesite un bien. 
+          </p>
         </div>
-           
-           
-    
-          </div>
 
+        <div className="flex flex-col justify-center items-center">
+        <img src={apreton} alt="corazon" className="w-[120px] h-[120px]" />
 
+          <h2 className="my-4 font-semibold text-4xl">¿Qué es una donación?</h2>
+          <p className=" md:w-44 ">
+          Acto dar un bien a otra persona de forma gratuita.
 
+          </p>
         </div>
-        
-        <div className=" bg-white h-[480px] w-[60%] content-center items-center ml-auto my-auto rounded-xl">
-          <div className="  m-auto  content-center ">
-          <h2 className=" text-7xl text-center m-auto font-bold leading-tight px-2 pt-5 pb-2 text-[#195527]">
-          ¿Qué es una donación?
-            </h2>
-            <p className=" text-2xl text-center font-semibold leading-tight py-7 px-[150px] p-1 text-[#195527]">
-              
-              La donación en vida, es un acto por el que una persona entrega y transmite la propiedad 
-              de un bien o dinero, normalmente de forma gratuita y sin nada a cambio, a otra persona, 
-              que la acepta.
-            </p>
-           
-           
-    
-          </div>
 
+        <div className="flex flex-col justify-center items-center ">
+          
 
-
+          <h2 className="my-4 font-semibold text-4xl">¿Por qué?</h2>
+          <p className=" md:w-44 ">
+            Debido a que no todas las personas tenemos los mismos bienes.
+          </p>
         </div>
-   </div>
-   
+      </div>
+    </div>
+     
 
-        
-    
-       
-      
+
+
     
     
-    
-      </section>
-    
+
+
+
     
     
     )
     
     }
     export default Donacion;
+
+
+
+
+
+   
